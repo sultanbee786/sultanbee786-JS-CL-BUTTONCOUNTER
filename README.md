309feabb76413a2e21386b6ef3db833e964cc262
sultanbee786 <sulthanbeeshaik31@gmail.com>
2025-01-21 20:37:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# JS Button Counter Lab
+
+
+## Description
+
+In this lab you'll learn how to use JavaScript and DOM Manipulation to alter HTML elements, then altering the HTML elements again using knowledge about previous alterations. In short, you'll want your HTML page to remember the work it has done for the purposes of future calculations.
+
+
+## Where to start
+
+Begin by opening and viewing the "ButtonCounter.html" file; the file itself contains this lab's instructions.
+
+Files to edit:
+- ButtonCounter.html
+
+
+
+
+
---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,73 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        // Set up ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
+
+        // Get file
+        File file = new File("src/main/ButtonCounter.html");
+        String path = "file://" + file.getAbsolutePath();
+
+        // Create a new ChromeDriver instance
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
+        // Open the HTML file
+        webDriver.get(path);
+    }
+
+    @Test
+    public void testIncrementButton() {
+
+        // Find the increment button and click it
+        WebElement incrementButton = webDriver.findElement(By.id("button"));
+        incrementButton.click();
+
+        // Find the display element and verify the incremented value
+        WebElement displayElement = webDriver.findElement(By.id("count"));
+        assertEquals("1", displayElement.getText());
+    }
+    @Test
+    public void testIncrementButtonMultipleTimes() {
+
+        // Find the increment button and click it
+        WebElement incrementButton = webDriver.findElement(By.id("button"));
+        incrementButton.click();
+        incrementButton.click();
+        incrementButton.click();
+        incrementButton.click();
+        incrementButton.click();
+        incrementButton.click();
+        
+
+        // Find the display element and verify the incremented value
+        WebElement displayElement = webDriver.findElement(By.id("count"));
+        assertEquals("6", displayElement.getText());
+
+    }
+
+
+
+    @After
+    public void tearDown() {
+        // Close the browser
+        webDriver.quit();
+    }
+}
